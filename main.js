628e399b38f6744e9086e71698e4156822e89e71
gaston bracalenti <gastonabracalenti@gmail.com>
2024-04-13 20:35:30 -0300

    Se cambia toda la interaccion del usuario, se agrega un archivo CSS para personalizar la pagina, se agrega una nueva interfaz que permite agragar cosas al carrito y hacer el pedido con un sistema de botones en vez de prompts

--- main.js
+++ main.js
@@ -10,93 +10,65 @@ let totalCarrito = 0
 let gomitas = new Producto ("Gomitas", 800);
 let chocolates = new Producto ("Chocolates", 1500);
 let alfajores = new Producto ("Alfajores", 3000);
+let gomitasVar = new Producto ("Gomitas Surtidas", 1200);
+let chocolatesVar = new Producto ("Chocolates Surtidos", 2250);
+let alfajoresVar = new Producto ("Alfajores Surtidos", 4500);
 
 
 const arrayProductos = [gomitas, chocolates, alfajores]
 
-function bienvenida(){
-    pedido = prompt("Bienvenido! Esta listo para hacer su pedido? SI/NO");
-    if(pedido.toLowerCase() === "no"){
-        alert("Gracias, vuelva pronto!")
-    } else{
-        comprar()
-    }
-}
-
-function comprar(){
-
-    orden = parseInt(prompt(`Que desea comprar:\n1) ${arrayProductos[0].nombre} \n2) ${arrayProductos[1].nombre}\n3) ${arrayProductos[2].nombre}`));
-
-    switch(orden){
-        case 1:
-            tipo = prompt("Quiere gomitas simples o surtidas?")
-            if(tipo.toUpperCase() === "SIMPLES"){
-                totalCarrito += gomitas.precio;
-                let sigue = confirm("Desea comprar algo mas?");
-                        if (sigue === false){
-                            alert(`Gracias por su compra! Su total es de $${totalCarrito}`)
-                }else{
-                    comprar()
-                }
-            }else{
-                totalCarrito += surtido(gomitas)
-                let sigue = confirm("Desea comprar algo mas?");
-                        if (sigue === false){
-                            alert(`Gracias por su compra! Su total es de $${totalCarrito}`)
-                }else{
-                    comprar()
-                }
-            }
-        break
-
-            case 2:
-                tipo = prompt("Quiere chocolates simples o surtidos?")
-                if(tipo.toUpperCase() === "SIMPLES"){
-                    totalCarrito += chocolates.precio;
-                    let sigue = confirm("Desea comprar algo mas?");
-                        if (sigue === false){
-                            alert(`Gracias por su compra! Su total es de $${totalCarrito}`)
-                    }else{
-                        comprar()
-                    }
-                }else{
-                    totalCarrito += surtido(alfajores)
-                    let sigue = confirm("Desea comprar algo mas?");
-                        if (sigue === false){
-                            alert(`Gracias por su compra! Su total es de $${totalCarrito}`)
-                    }else{
-                        comprar()
-                    }
-                }
-            break
-
-                case 3:
-                    tipo = prompt("Quiere alfajores simples o surtidos?")
-                    if(tipo.toUpperCase() === "SIMPLES"){
-                        totalCarrito += alfajores.precio;
-                        let sigue = confirm("Desea comprar algo mas?");
-                        if (sigue === false){
-                            alert(`Gracias por su compra! Su total es de $${totalCarrito}`)
-                            break
-                        }else{
-                            comprar()
-                        }
-                    }else{
-                        totalCarrito += surtido(alfajores)
-                        let sigue = confirm("Desea comprar algo mas?");
-                        if (sigue === false){
-                            alert(`Gracias por su compra! Su total es de $${totalCarrito}`)
-                        }else{
-                            comprar()
-                        }
-                    }
-                break
-    }
-    }
-
 function surtido(item){
-    itemsurtido = item.precio*1.4;
+    itemsurtido = item.precio*1.5;
     return itemsurtido
 }
 
-bienvenida()
+const addGomitas = document.getElementById("gom-cla");
+const addGomitasVar = document.getElementById("gom-var");
+const addAlfa = document.getElementById("alfa-cla");
+const addAlfaVar = document.getElementById("alfa-var");
+const addChoc = document.getElementById("choc-cla");
+const addChocVar = document.getElementById("choc-var");
+const pedir = document.getElementById("finCompra");
+const addCarrito = document.getElementById("carrito");
+const addCantCarrito = document.getElementById("cantCarrito");
+const addPrecio = document.getElementById("precio");
+
+let itemsCarrito = []
+
+addGomitas.addEventListener("click", () =>{
+    agregarAlCarrito(gomitas);
+});
+addGomitasVar.addEventListener("click", () =>{
+    agregarAlCarrito(gomitasVar);
+});
+addAlfa.addEventListener("click", () =>{
+    agregarAlCarrito(alfajores);
+});
+addAlfaVar.addEventListener("click", () =>{
+    agregarAlCarrito(alfajoresVar);
+});
+addChoc.addEventListener("click", () =>{
+    agregarAlCarrito(chocolates);
+});
+addChocVar.addEventListener("click", () =>{
+    agregarAlCarrito(chocolatesVar);
+});
+
+pedir.addEventListener("click", () =>{
+    alert(`Pedido realizado! Tu total fue de $${totalCarrito}`)
+    itemsCarrito = [];
+    addCarrito.textContent = "";
+    totalCarrito = 0
+    actualizarTotal()
+})
+
+function agregarAlCarrito(item){
+    totalCarrito += item.precio;
+    itemsCarrito.push(item);
+    addCarrito.textContent += "\n"+`${item.nombre}`
+    actualizarTotal();
+}
+
+function actualizarTotal(){
+    addPrecio.textContent = `${totalCarrito}`
+}
